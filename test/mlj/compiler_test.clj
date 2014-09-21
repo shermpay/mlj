29ad77c532fe9780fec4bacb6689002bb72a0a4f
shermpay <shermanpay1991@gmail.com>
2014-09-20 23:48:38 -0700

    Added tests for compiler

--- test/mlj/compiler_test.clj
+++ test/mlj/compiler_test.clj
@@ -3,15 +3,47 @@
             [mlj.compiler :as comp]
             [mlj.lang :as ml]))
 
-(deftest typesig-test
-  (testing "typesig parsing"
+(deftest mlj-parser-test
+  (is (= (comp/parse-res "comp.sml")
+         '([:decl [:val [:id "x"] [:expr [:number "1"]]]]
+             [:decl [:val [:id "foo"] [:expr [:fn [:id "x"] [:expr [:id "x"]]]]]]
+               [:decl [:val [:id "y"] [:expr [:id "foo"] [:string "a"]]]]
+                 [:decl [:val [:id "z"] [:expr [:id "foo"] [:char "#" "\"" "a" "\""]]]]
+                   [:decl
+                    [:val
+                     [:id "a"]
+                     [:expr
+                      [:if
+                       [:expr [:bool "true"]]
+                       [:expr [:number "1"]]
+                       [:expr [:number "2"]]]]]]
+                     [:decl
+                      [:val
+                       [:id "b"]
+                       [:expr
+                        [:if
+                         [:expr [:bool "true"]]
+                         [:expr
+                          [:if
+                           [:expr [:bool "true"]]
+                           [:expr [:number "1"]]
+                           [:expr [:number "2"]]]]
+                         [:expr [:number "3"]]]]]]
+                       [:decl [:val [:id "t"] [:expr [:bool "true"]]]]
+                         [:decl [:val [:id "f"] [:expr [:bool "false"]]]]
+                           [:decl
+                            [:val [:id "l"] [:expr [:let "in" [:expr [:number "1"]] "end"]]]]
+                             [:decl [:val [:id "tt" [:ann [:type "bool"]]] [:expr [:bool "true"]]]]))))
+
+;; (deftest typesig-test
+;;   (testing "typesig parsing"
     
-    (is (= (comp/parse-fn-typesig '([:int * :int] -> :string -> :char -> :int))
-           '([:int * :int] (:string (:char :int)))) "fn typesig parsing")
-    (are [sig r] (= (comp/parse-typesig sig) r)
-         :int :int
-         '[:int * :char] [:int :char]
-         '([:int * :char] -> [:char * :int]) '([:int :char] [:char :int]))))
+;;     (is (= (comp/parse-fn-typesig '([:int * :int] -> :string -> :char -> :int))
+;;            '([:int * :int] (:string (:char :int)))) "fn typesig parsing")
+;;     (are [sig r] (= (comp/parse-typesig sig) r)
+;;          :int :int
+;;          '[:int * :char] [:int :char]
+;;          '([:int * :char] -> [:char * :int]) '([:int :char] [:char :int]))))
 
 ;; (deftest mlj-compile-test
 ;;   (is (= (comp/parse if true then 1 else 2) '((#'mlj.lang/if true then 1 else 2)) ))
