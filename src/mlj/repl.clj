011f98d6fe5df31fab11fe76d2c93824a541f9d2
shermpay <shermanpay1991@gmail.com>
2014-11-29 19:14:26 -0800

    Working repl. Removed legacy type.clj

--- src/mlj/repl.clj
+++ src/mlj/repl.clj
@@ -1,7 +1,8 @@
 (ns mlj.repl
   "REPL for MLJ lang"
   (:require [mlj.compiler :as compiler]
-            [mlj.parser :as parser]))
+            [mlj.parser :as parser]
+            [mlj.type :as type]))
 
 (defn intro
   "Prints an intro message."
@@ -10,25 +11,38 @@
   (println "Type exit to quit."))
 
 (def ^:dynamic *prompt* "- ")
-(def ^:dynamic *output* "=")
+(def ^:dynamic *output* ">")
 
 (defn ml-eval
   "Eval forms. If symbol hold function return it's function signature,
   else return it's value."
   [exp]
-  (if (ifn? exp)
+  (if (and (ifn? exp) ((complement vector?) exp))
     (eval `(meta (var ~exp)))
     (eval exp)))
 
+(defn handle-decl
+  "Takes a declaration outputs to stdout and returns it's environment"
+  [decl env]
+  (let [new-env (type/decl-id decl env)
+        v (eval (compiler/compile decl))]
+    (doseq [[sym t] new-env]
+      (println *output* "val" sym "=" (var-get v) t))
+    new-env))
+
+(defn handle-expr
+  [expr env]
+  (let [t (type/type-of expr env)
+        v (eval (compiler/compile expr))]
+    (println *output* "val it =" v t)))
+
 (defn eval-print
   "Eval and print an input string."
-  [input]
-  (->> input
-       (parser/parse)
-       (first)
-       (compiler/compile)
-       (ml-eval)
-       (println *output*)))
+  [input env]
+  (let [parse-tree (->> input (parser/parse) (first))]
+    (case (first parse-tree)
+      :decl (handle-decl parse-tree env)
+      :expr (handle-expr parse-tree env))))
 
 (defn prompt-read
   "Prints a prompt and reads a string of input."
@@ -40,8 +54,9 @@
 (defn main []
   (intro)
   (refer 'mlj.lib)
-  (loop [input (prompt-read)]
+  (loop [env {}
+         input (prompt-read)]
     (if (or (= input "exit") (nil? input))
       (println "Exiting MLJ.")
-      (do (eval-print input)
-          (recur (prompt-read))))))
+      (recur (conj env (eval-print input env))
+             (prompt-read)))))
--- src/mlj/type.clj
+++ src/mlj/type.clj
@@ -30,211 +30,12 @@
                                   :type-fn char?}
                            :unit {:type nil
                                   :type-fn unit?}})
-;;;;;;;;;;;;;;;;;;;;
-;; Type Variables ;;
-;;;;;;;;;;;;;;;;;;;;
-;; (def type-vars (-> (map (comp keyword str char) (range (int \a) (int \z)))
-;;                    vec
-;;                    atom))
-;; (defn type-var?
-;;   [t]
-;;   {:pre [(keyword? t)]}
-;;   (and (not (*type-map* t))))
 
 ;;;;;;;;;;;;;;;;;
 ;; Tuple Types ;;
 ;;;;;;;;;;;;;;;;;
 (defn tuple? [v] (vector? v))
 (defn unit? [v] (and (tuple? v) (empty? v)))
-
-;; (defn tuple-type
-;;   [tuple env]
-;;   {:pre [(tuple? tuple)]}
-;;   (vec (map #(type-of % env) tuple)))
-
-;; (defn tuple-repr
-;;   [tuple]
-;;   "Converts a tuple-type obj to it's external representation. [:int :int] to [:int * :int]"
-;;   (vec (interpose '* tuple)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Type checking primitives ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (defn type? 
-;;   "Returns true if t is a type form."
-;;   [t]
-;;   (if (tuple? t)
-;;     (not-any? false? (map type? t))
-;;     (contains? *type-map* t)))
-
-;; (defn var-type
-;;   "Gets the type of a var"
-;;   [vr]
-;;   (-> vr
-;;       core/get-var
-;;       meta
-;;       :type))
-
-;; (defn type-of 
-;;   "Gets the type of a value"
-;;   [v env]
-;;   {:pre [(not (keyword? v)) (not (list? v))]}
-;;   (cond
-;;    (nil? v) :unit
-;;    (tuple? v) (tuple-type v env)
-;;    (var? v) (var-type v)
-;;    (symbol? v) (env v)
-;;    :else (-> (filter #((:type-fn (% 1)) v) *type-map*)
-;;              first
-;;              key)))
-
-;; (defn check-type 
-;;   "Check if v is of type t in env"
-;;   [v t env]
-;;   (= (type-of v env) t))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Function Type signatures ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (defn fn-type?
-;;   "Checks if is a function type"
-;;   [vr]
-;;   (= :fn (core/typesig-type (var-type vr))))
-
-;; (defn fn-type
-;;   "Gets the type-signature of a function"
-;;   [f]
-;;   {:pre [(core/builtin? f)]}
-;;   (core/get-type f))
-
-;;;;;;;;;;;;;;;;;;;;;;;
-;; Type Environments ;;
-;;;;;;;;;;;;;;;;;;;;;;;
-;; (defn make-default-env
-;;   "Create a default static environment {symbol type-sig}"
-;;   []
-;;   (reduce (fn [res sym]
-;;             (assoc res sym (core/get-type sym)))
-;;           {}
-;;           (keys (core/op-map :builtin))))
-
-;; ;;; atom should NOT be modified for lexical reasons, only modifications are top-level definitions
-;; (def environment "Environment of {symbol type-sig}" (atom (make-default-env)))
-
-;; (defn fn-env
-;;   "Creates a lexical environment for a function"
-;;   [params param-types]
-;;   (if (symbol? params)
-;;     (hash-map params param-types)
-;;     (zipmap params param-types)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Type checking forms ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Application should hold a type variable map, that maps a type variable to a type
-;; (defn check-app
-;;   "Type checks a function application. Returns the return type of the function if valid."
-;;   [[f arg] env]
-;;   (let [[par ret] (fn-type f)
-;;         arg-types (if (tuple? arg)
-;;                     (vec (map-indexed (fn [idx item] (if-let [t (check-expr item env)]
-;;                                                        t
-;;                                                        (let [sym (arg idx)
-;;                                                              infered (par idx)]
-;;                                                          (swap! env assoc sym infered)
-;;                                                          infered)))
-;;                                       arg))
-;;                     (check-expr arg @env))]
-;;     (if (not= arg-types par)
-;;       (throw (IllegalArgumentException.
-;;               (str "Type Error: " f " args: (" arg "), expecting: " par "\n Got: " arg-types)))
-;;       ret)))
-
-;; (defn check-if
-;;   "Type checks an IF expression. Returns the type of the branches if valid, else throws an exception"
-;;   [[if-sym pred then-sym t-expr else-sym e-expr :as if-expr] env]
-;;   (do
-;;     (if-let [pred-type (check-expr pred env)]
-;;         (if (not= pred-type :bool)
-;;           (throw (IllegalArgumentException.
-;;                   (str "Type Error: test in IF expression not type bool"
-;;                        "\n\ttest expression: " (type-of pred @env)
-;;                        "\n\tin expression: " if-expr))))
-;;         (swap! env assoc pred :bool))
-;;     (let [t-type (check-expr t-expr env)
-;;           e-type (check-expr e-expr env)
-;;           t-type (if (and (nil? t-type) (not (nil? e-type)))
-;;                    e-type t-type)
-;;           e-type (if (and (nil? e-type) (not (nil? t-type)))
-;;                    t-type e-type)]
-;;       (if (symbol? t-expr) (swap! env assoc t-expr t-type))
-;;       (if (symbol? e-expr) (swap! env assoc e-expr e-type))
-;;       (if (= t-type e-type)
-;;         t-type
-;;         (throw (IllegalArgumentException.
-;;                 (str "Type Error: IF branches types do not agree." t-type " <> " e-type)))))))
-;; (defn- set-type! [sym t env]
-;;   (do (swap! env assoc sym t)
-;;       t))
-
-;; (defn check-val
-;;   "Type checks a VAL declaration"
-;;   ([[val-sym sym & [& {tsig :- v :=}] :as val-expr] env]
-;;      (let [t (check-expr v env)]
-;;        (if (or (nil? tsig) (= tsig t))
-;;          (do
-;;            (swap! env assoc sym t)
-;;            t)
-;;          (throw (IllegalArgumentException.
-;;                  (str "Type Error: VAL binding type mismatch."
-;;                       "\n\tgot: " (type-of v @env)
-;;                       "\n\tin: " (apply str (interpose \space val-expr)))))))))
-
-;; (defn check-fn
-;;   "Type checks a FN expression. Return the fn type if valid"
-;;   [[fn-sym param & {[par-type ret-type :as tsig] :-, body :=>}] env]
-;;   (let [env (atom (merge env (fn-env param par-type)))
-;;         body-type (check-expr body env)
-;;         par-type (if (nil? par-type) (check-expr param env) par-type)]
-;;     (if (or (nil? ret-type) (= body-type ret-type)) 
-;;       (list par-type body-type)
-;;       (throw (IllegalArgumentException.
-;;               (str "Type Error: FN return type mismatch."
-;;                    "\n\tExpected: " ret-type
-;;                    "\n\tGot: " body-type))))))
-
-;; (defn check-fun
-;;   "Type checks a FUN declaration. Return the fn type if valid"
-;;   [[fun-sym name param & {[par-type ret-type :as tsig] :-, body :=}] env]
-;;   (check-val ['val name :- tsig := (list 'fn param :- tsig :=> body)] env))
-
-;; (defn check-let
-;;   "Type check a LET expression. Returns the type of the body if valid"
-;;   [[let-sym binding-exprs & {body :in} :as let-expr] env]
-;;   (let [let-env (atom env)]
-;;     (doseq [binding (keep-indexed #(if (odd? %1) (conj %2 'val))
-;;                                   (partition-by #(= :val %) binding-exprs))]
-;;       (check-val binding let-env))
-;;     (check-expr body let-env)))
-
-;; (defn check-expr
-;;   "Type checks an expression. 
-;;   Throws an exception if type is invalid, else returns the type of the expression"
-;;   [expr env]
-;;   (if (list? expr)
-;;     (let [[form & body] expr]
-;;       (cond
-;;        (core/mlj-keyword? form) (case form
-;;                                   val (check-val expr environment)
-;;                                   if (check-if expr env)
-;;                                   let (check-let expr @env)
-;;                                   fn (check-fn expr @env)
-;;                                   fun (check-fun expr environment)
-;;                                   (throw (IllegalStateException. (str "Found uncheck keyword: " form))))
-;;        (core/builtin? form) (check-app expr env)
-;;        :else ((throw (IllegalStateException. (str "Found uncheck form: " form))))))
-;;     (type-of expr @env)))
-
 
 (declare decl-val create-env pat->vec)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -257,7 +58,7 @@
 (defmethod type-of :bool [_ env]
   :bool)
 
-(defmethod type-of :tuple [[_ tuple] env]
+(defmethod type-of :tuple [[_ & tuple] env]
   (mapv #(type-of % env) tuple))
 
 (defmethod type-of :id [[_ sym] env]
@@ -291,7 +92,7 @@
     [:fn param-types ret-type]))
 
 (defmethod type-of :default [_ env]
-  (println _))
+  (throw (ex-info "Unable to obtain types." {:given _})))
 
 (defn create-env [pat env]
   (reduce #(conj %1 (decl-val %2 env)) env pat))
@@ -320,32 +121,6 @@
 
 (defn check-ann [[node ann]]
   (= (type-of node) ann))
-;;;;;;;;;;;;;;;;;
-;; Expressions ;;
-;;;;;;;;;;;;;;;;;
-;; (defmethod check :expr [[_ exp & [ann]] env]
-;;   (if (check exp env)
-;;     (match-ann exp ann)
-;;     (throw (ex-info "Type mismatch." {:received exp
-;;                                       :expected ann}))))
-
-;; (defmethod check :tuple [tuple env]
-;;   (let [type-tuple (map #(check % env) tuple)]
-;;     type-tuple))
-
-;; (defmethod check :if [[_ test then else :as if-expr] env]
-;;   (let [test-type (check test env)
-;;         then-type (check then env)
-;;         else-type (check else env)]
-;;     (if (not= test-type :bool)
-;;       (throw (ex-info "test expr in if is not type bool" {:test test-type
-;;                                                           :expr if-expr}))
-;;       (if (not= then-type else-type)
-;;         (throw (ex-info "then and else expr in if type mismatch" {:then then-type
-;;                                                                   :else else-type
-;;                                                                   :expr if-expr}))
-;;         then-type))))
-
 ;;;;;;;;;;;;;;;;;;
 ;; Declarations ;;
 ;;;;;;;;;;;;;;;;;;
@@ -362,7 +137,7 @@
   ;; pairs ("x" :int, "y" :int)
   ;; actual (:int :int)
   (let [pairs (flatten (pat->vec pat))
-        actual (vector (type-of exp env))]
+        actual (flatten (vector (type-of exp env)))]
     (loop [[id type & more-pairs] pairs
            [t & exp-types] actual
            result {}]
@@ -370,8 +145,8 @@
         result
         (if (not= t type)
           (throw (ex-info "Pattern and expression in val dec don't agree."
-                                {:pattern type
-                                 :expression t}))
+                          {:pattern type
+                           :expression t}))
           (recur more-pairs exp-types (conj result [id t])))))))
 
 (defn decl-id
@@ -380,6 +155,15 @@
   [[form body] env]
   {:pre (= :decl form)}
   (decl-val body env))
+
+(defn teval
+  "Takes a parse-tree and does the following:
+  If it is a declaration => return a map of it's new environment.
+  If it is an expression => return it's type."
+  [parse-tree]
+  (case (ast/tag-of parse-tree)
+    :expr (type-of parse-tree)
+    :decl (decl-id parse-tree)))
 
 ;; (defmethod check :decl [[_ & [more]] env]
 ;;   (check more env))
